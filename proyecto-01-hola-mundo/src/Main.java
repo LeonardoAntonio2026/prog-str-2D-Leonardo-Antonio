b7f8afec46174feab458acce99d93ac3c71b3d85
LeoManzana <leomanzana07@gmail.com>
2026-01-13 18:32:55 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,8 @@
+
+public static void main(String[] args) {
+    System.out.println("Hola mundo 🍏🍏🍏🍏🍏🍏🍏🍏");
+    System.out.println("Leonardo Antonio Arroyo Rodriguez");
+    System.out.println("2D");
+    System.out.println("13/01/2026");
+    System.out.println("Hola mundo");
+}
